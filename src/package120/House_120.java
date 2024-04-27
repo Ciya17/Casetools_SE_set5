78158f62c2ce607c68273b2b8972f62b1b32a7c2
ciya17 <ciyasilva1278@gmail.com>
2024-04-27 09:22:58 +0530

    commit casetools_see_exam

---
+++ src/package120/House_120.java
@@ -0,0 +1,15 @@
+package package120;
+
+public class House_120 {
+	String color;
+	String Owner;
+		House_120(String color,String owner)
+		{
+			this.color=color;
+			this.Owner=owner;
+		}
+		String getOwner() {
+			return Owner;
+		}
+
+}
